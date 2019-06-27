752d77530cd5ccbd1f56725f4f0db4dd5658c5da
jgould <jgould@broadinstitute.org>
2019-06-27 15:35:34 -0400

    use docker tags

--- docker/SmartSeq2/build.sh
+++ docker/SmartSeq2/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker build -t smartseq2-0.2.0 .
-docker tag smartseq2-0.2.0 regevlab/smartseq2-0.2.0
-docker push regevlab/smartseq2-0.2.0
+docker tag smartseq2-0.2.0 regevlab/smartseq2:0.2.0
+docker push regevlab/smartseq2:0.2.0
--- docker/cellranger/2.0.2/build.sh
+++ docker/cellranger/2.0.2/build.sh
@@ -2,5 +2,5 @@
 
 # Download cellranger-2.0.2.tar into this directory from cell ranger website before building
 docker build -t cellranger-2.0.2 .
-docker tag cellranger-2.0.2 regevlab/cellranger-2.0.2
-docker push regevlab/cellranger-2.0.2
+docker tag cellranger2.0.2 regevlab/cellranger:2.0.2
+docker push regevlab/cellranger:2.0.2
