38094114679a3e68da2a649524e3af48c9fc5061
jgould <jgould@broadinstitute.org>
2019-08-19 12:08:41 -0400

    use sccloud docker

--- docker/bcl2fastq/2.20.0.422/build.sh
+++ docker/bcl2fastq/2.20.0.422/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 docker build -t bcl2fastq-2.20.0.422 .
-docker tag bcl2fastq-2.20.0.422 regevlab/bcl2fastq:2.20.0.422
-docker push regevlab/bcl2fastq:2.20.0.422
+docker tag bcl2fastq-2.20.0.422 sccloud/bcl2fastq:2.20.0.422
+docker push sccloud/bcl2fastq:2.20.0.422
 
