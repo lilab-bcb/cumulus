59b81434ce37a206c9cb54aa939d5bc232e7e7ee
jgould <jgould@broadinstitute.org>
2019-08-27 12:13:43 -0400

    Removed

--- docker/cellranger-atac/1.0.1/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Download cellranger-atac-1.0.1.tar.gz into this directory from cell ranger website before building
-docker build -t cellranger-atac-1.0.1 .
-docker tag cellranger-atac-1.0.1 regevlab/cellranger-atac:1.0.1
-docker push regevlab/cellranger-atac:1.0.1
