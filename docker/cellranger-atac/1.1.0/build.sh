58434f98b6630dd0c9c167460a5a0be9f48cb6a5
jgould <jgould@broadinstitute.org>
2019-08-27 12:05:34 -0400

    use sccloud docker organization

--- docker/cellranger-atac/1.1.0/build.sh
+++ docker/cellranger-atac/1.1.0/build.sh
@@ -2,5 +2,5 @@
 
 # Download cellranger-atac-1.1.0.tar.gz into this directory from cell ranger website before building
 docker build -t cellranger-atac-1.1.0 .
-docker tag cellranger-atac-1.1.0 regevlab/cellranger-atac:1.1.0
-docker push regevlab/cellranger-atac:1.1.0
+docker tag cellranger-atac-1.1.0 sccloud/cellranger-atac:1.1.0
+docker push sccloud/cellranger-atac
