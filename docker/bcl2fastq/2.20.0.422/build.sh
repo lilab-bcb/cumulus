8f644e82b3f1ef337acf5f8e2fe8bcc5052711c1
jgould <jgould@broadinstitute.org>
2019-09-16 08:48:56 -0400

    Updated dockers

--- docker/bcl2fastq/2.20.0.422/build.sh
+++ docker/bcl2fastq/2.20.0.422/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 docker build -t bcl2fastq-2.20.0.422 .
-docker tag bcl2fastq-2.20.0.422 sccloud/bcl2fastq:2.20.0.422
-docker push sccloud/bcl2fastq:2.20.0.422
+docker tag bcl2fastq-2.20.0.422 gcr.io/sccloud-prod/bcl2fastq:2.20.0.422
+docker push gcr.io/sccloud-prod/bcl2fastq:2.20.0.422
+
 
