207266cf1ba68e0aade799a20e2f9fbd8df66deb
Yiming Yang <yang.yihming@gmail.com>
2019-09-18 00:48:58 -0400

    build sccloud 0.9.1

--- docker/scCloud/build.sh
+++ docker/scCloud/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t sccloud-0.9.0 .
-docker tag sccloud-0.9.0 sccloud/sccloud:0.9.0
+docker build -t sccloud-0.9.1 .
+docker tag sccloud-0.9.1 sccloud/sccloud:0.9.1
 docker push sccloud/sccloud
