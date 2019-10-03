fb3fe0a6fa346de1ca2303a825df55b02a77fc9d
Yiming Yang <yang.yihming@gmail.com>
2019-10-02 23:55:24 -0400

    h5ad to seurat v3

--- workflows/cumulus/h5ad2seurat.R
+++ workflows/cumulus/h5ad2seurat.R
@@ -1,102 +1,147 @@
-### This script is modified from conversion.R of Seurat's github. Seurat is licensed under GPL 3.0
+### This script is modified from conversion.R of Seurat V2 source code. Seurat is licensed under GPL 3.0
 
-
-#' @param X.slot Seurat slot to transfer anndata X into. Default is scale.data
-#' @param raw.slot Seurat slot to transfer anndata raw into. Default is data
 #' @describeIn Convert from Anndata file to a Seurat object
 #' @importFrom reticulate py_to_r
-#' @export
-#' @method Convert anndata.base.AnnData
+#' @export Seurat, Matrix, reticulate
+#' @method convert_h5ad_to_seurat
 #'
 #'
-Convert.anndata.base.AnnData <- function(
-	from,
-	to,
-	...
-) {
-	object.to <- switch(
-		EXPR = to,
-		'seurat' = {
-			raw.data.matrix <- sparseMatrix(
-				i = as.numeric(x = from$raw$X$indices),
-				p = as.numeric(x = from$raw$X$indptr),
-				x = as.numeric(x = from$raw$X$data),
-				index1 = FALSE
-			)
-			print("GOOD!")
-			rownames(x = raw.data.matrix) <- rownames(x = py_to_r(from$raw$var))
-			colnames(x = raw.data.matrix) <- rownames(x = py_to_r(from$obs))
-			meta.data <- py_to_r(from$obs)
-			if ("n_counts" %in% colnames(x = meta.data)) {
-				colnames(x = meta.data) <- gsub(
-					pattern = "n_counts",
-					replacement = "nUMI",
-					x = colnames(x = meta.data)
-				)
-			}
-			if ("n_genes" %in% colnames(x = meta.data)) {
-				colnames(x = meta.data) <- gsub(
-					pattern = "n_genes",
-					replacement = "nGene",
-					x = colnames(x = meta.data)
-				)
-			}
-			seurat.object <- CreateSeuratObject(
-				raw.data = raw.data.matrix,
-				meta.data = meta.data
-			)
 
-			data.matrix <- sparseMatrix(
-				i = as.numeric(x = from$X$indices),
-				p = as.numeric(x = from$X$indptr),
-				x = as.numeric(x = from$X$data),
-				index1 = FALSE
-			)
-			rownames(x = data.matrix) <- rownames(x = py_to_r(from$var))
-			colnames(x = data.matrix) <- rownames(x = py_to_r(from$obs))
-			seurat.object <- SetAssayData(
-				object = seurat.object,
-				assay.type = "RNA",
-				slot = "data",
-				new.data = data.matrix
-			)
+library(Seurat)
+library(Matrix)
+library(reticulate)
 
-			scale.data.matrix <- t(x = py_to_r(from$uns["scale.data"]))
-			rownames(x = scale.data.matrix) <- py_to_r(from$uns["scale.data.rownames"])
-			colnames(x = scale.data.matrix) <- rownames(x = py_to_r(from$obs))
-			seurat.object <- SetAssayData(
-				object = seurat.object,
-				assay.type = "RNA",
-				slot = "scale.data",
-				new.data = scale.data.matrix
-			)
+convert_h5ad_to_seurat <- function(from) {
+	isV3 <- startsWith(toString(packageVersion("Seurat")), "3.")
 
-			dr.dict <- list(tSNE_ = "tsne", PC = "pca", UMAP = "umap", FLE = "fle", DIFFMAP_PCA = "diffmap_pca")
-			drs <- unlist(x = py_to_r(from$obsm$keys()))
-			for (dr.key in names(dr.dict)) {
-				dr.name <- dr.dict[[dr.key]]
-				dr.pkey <- paste0("X_", dr.name)
-				dr.embed <- py_to_r(from$obsm$get(dr.pkey))
-				if (!is.null(dr.embed)) {
-					colnames(x = dr.embed) <- paste0(dr.key, 1:ncol(x = dr.embed))
-					rownames(x = dr.embed) <- seurat.object@cell.names
-					seurat.object <- SetDimReduction(
-						object = seurat.object,
-						reduction.type = dr.name,
-						slot = "cell.embeddings",
-						new.data = dr.embed
-					)
-					seurat.object <- SetDimReduction(
-						object = seurat.object,
-						reduction.type = dr.name,
-						slot = "key",
-						new.data = dr.key
-					)
-				}
-			}
-			seurat.object
-		},
-		stop(paste0("Cannot convert AnnData objects to class '", to, "'"))
+	meta.data <- py_to_r(from$obs)
+	for (key in colnames(meta.data)) {
+		if (from$obs[key]$dtype$name == "category") {
+			meta.data[key] = py_to_r(from$obs[key]$astype("str"))
+		}
+	}
+	if ("n_counts" %in% colnames(x = meta.data)) {
+		colnames(x = meta.data) <- gsub(
+			pattern = "n_counts",
+			replacement = "nUMI",
+			x = colnames(x = meta.data)
+		)
+	}
+	if ("n_genes" %in% colnames(x = meta.data)) {
+		colnames(x = meta.data) <- gsub(
+			pattern = "n_genes",
+			replacement = "nGene",
+			x = colnames(x = meta.data)
+		)
+	}
+
+	meta.features <- py_to_r(from$var)
+	for (key in colnames(meta.features)) {
+		if (from$var[key]$dtype$name == "category") {
+			meta.features[key] = py_to_r(from$var[key]$astype("str"))
+		}
+	}
+
+	raw.data.matrix <- sparseMatrix(
+		i = as.numeric(x = from$raw$X$indices),
+		p = as.numeric(x = from$raw$X$indptr),
+		x = as.numeric(x = from$raw$X$data),
+		index1 = FALSE
 	)
-	return(object.to)
+	rownames(x = raw.data.matrix) <- rownames(x = py_to_r(from$raw$var))
+	colnames(x = raw.data.matrix) <- rownames(x = meta.data)
+
+	if (isV3) {
+		seurat.object <- CreateSeuratObject(
+			counts = raw.data.matrix,
+			meta.data = meta.data,
+		)
+		tmp = seurat.object[["RNA"]]
+		tmp[[names(x = meta.features)]] = meta.features
+		seurat.object[["RNA"]] = tmp
+	} else {
+		seurat.object <- CreateSeuratObject(
+			raw.data = raw.data.matrix,
+			meta.data = meta.data
+		)
+	}
+
+	data.matrix <- sparseMatrix(
+		i = as.numeric(x = from$X$indices),
+		p = as.numeric(x = from$X$indptr),
+		x = as.numeric(x = from$X$data),
+		index1 = FALSE
+	)
+	rownames(x = data.matrix) <- rownames(x = meta.features)
+	colnames(x = data.matrix) <- rownames(x = meta.data)
+
+	if (isV3) {
+		seurat.object <- SetAssayData(
+			object = seurat.object,
+			slot = "data",
+			new.data = data.matrix
+		)
+	} else {
+		seurat.object <- SetAssayData(
+			object = seurat.object,
+			assay.type = "RNA",
+			slot = "data",
+			new.data = data.matrix
+		)
+	}
+
+	scale.data.matrix <- t(x = py_to_r(from$uns["scale.data"]))
+	rownames(x = scale.data.matrix) <- py_to_r(from$uns["scale.data.rownames"])
+	colnames(x = scale.data.matrix) <- rownames(x = meta.data)
+
+	if (isV3) {
+		seurat.object <- SetAssayData(
+			object = seurat.object,
+			slot = "scale.data",
+			new.data = scale.data.matrix
+		)
+	} else {
+		seurat.object <- SetAssayData(
+			object = seurat.object,
+			assay.type = "RNA",
+			slot = "scale.data",
+			new.data = scale.data.matrix
+		)
+	}
+
+	obsm_keys <- toString(test_ad$obsm$keys())
+	obsm_keys <- gsub("KeysView(AxisArrays with keys: ", "", obsm_keys, fixed = TRUE)
+	obsm_keys <- substr(obsm_keys, 1, nchar(obsm_keys) - 1)
+	obsm_keys <- strsplit(obsm_keys, split = ", ", fixed = TRUE)[[1]]
+
+	for (embed.key in obsm_keys) {
+		if (startsWith(embed.key, "X_")) {
+			embed.type <- substr(embed.key, 3, nchar(embed.key))
+			embed.name <- switch(embed.type, "pca" = "PC", "tsne" = "tSNE", toupper(embed.type))
+			embed.name <- paste0(embed.name, "_")
+			if (!isV3 && embed.name == "PC_") { embed.name = "PC" }
+			embed.content = py_to_r(from$obsm$get(embed.key))
+			colnames(x = embed.content) <- paste0(embed.name, 1:ncol(x = embed.content))
+			rownames(x = embed.content) <- rownames(x = meta.data)
+			if (isV3) {
+				dim.reduc <- CreateDimReducObject(embeddings = embed.content, assay = "RNA", key = embed.name)
+				seurat.object[[embed.type]] <- dim.reduc
+			} else {
+				seurat.object <- SetDimReduction(
+					object = seurat.object,
+					reduction.type = embed.type,
+					slot = "cell.embeddings",
+					new.data = embed.content
+				)
+				seurat.object <- SetDimReduction(
+					object = seurat.object,
+					reduction.type = embed.type,
+					slot = "key",
+					new.data = embed.name
+				)
+			}
+		}
+	}
+
+	return(seurat.object)
 }
