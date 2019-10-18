2a567f45b728583af751455ebbaa3e676336d23d
jgould <jgould@broadinstitute.org>
2019-10-18 08:06:17 -0400

    use DOCKER environment variable

--- docker/cumulus/build.sh
+++ docker/cumulus/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --no-cache -t cumulusprod-0.10.0 .
-docker tag cumulusprod-0.10.0  "$DOCKER"/cumulusprod:0.10.0
-docker push "$DOCKER"/cumulusprod:0.10.0
+docker build --no-cache -t cumulus-0.10.0 .
+docker tag cumulus-0.10.0 "$DOCKER"/cumulus:0.10.0
+docker push "$DOCKER"/cumulus:0.10.0
--- docker/dropest/0.8.6/build.sh
+++ docker/dropest/0.8.6/build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 docker build -t dropest-0.8.6 .
-docker tag dropest-0.8.6 cumulusprod/dropest:0.8.6
-docker push cumulusprod/dropest:0.8.6
-
+docker tag dropest-0.8.6 "$DOCKER"/dropest:0.8.6
+docker push "$DOCKER"/dropest:0.8.6
