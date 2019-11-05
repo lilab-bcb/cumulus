9c5119126c91dc4e0ec1e5db11bea2b37a5ec7d2
Yiming Yang <yang.yihming@gmail.com>
2019-11-05 15:03:28 -0500

    modify scripts

--- docker/cumulus/0.10.0/build.sh
+++ docker/cumulus/0.10.0/build.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
 docker build -t cumulus-0.10.0 .
-docker tag cumulus-0.10.0 cumulusprod/cumulus:0.10.0
-docker login
-docker push cumulusprod/cumulus:0.10.0
-docker tag cumulus-0.10.0 quay.io/cumulus/cumulus:0.10.0
-docker login quay.io
-docker push quay.io/cumulus/cumulus:0.10.0
--- docker/cumulus/test/build.sh
+++ docker/cumulus/test/build.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
 docker build -t cumulus-test .
-docker tag cumulus-test cumulusprod/cumulus:test
-docker login
-docker push cumulusprod/cumulus:test
-docker tag cumulus-test quay.io/cumulus/cumulus:test
-docker login quay.io
-docker push quay.io/cumulus/cumulus:test
