aa748054c1a50f479185d74ca3c62a44c933704b
Yiming Yang <yang.yihming@gmail.com>
2019-11-05 14:51:31 -0500

    update cumulus 0.10.0

--- docker/cumulus/0.10.0/build.sh
+++ docker/cumulus/0.10.0/build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 docker build -t cumulus-0.10.0 .
-docker tag cumulus-test cumulusprod/cumulus:0.10.0
+docker tag cumulus-0.10.0 cumulusprod/cumulus:0.10.0
+docker login
 docker push cumulusprod/cumulus:0.10.0
-#docker tag cumulus-test quay.io/cumulus:0.10.0
-#docker push quay.io/cumulus:0.10.0
+docker tag cumulus-0.10.0 quay.io/cumulus/cumulus:0.10.0
+docker login quay.io
+docker push quay.io/cumulus/cumulus:0.10.0
