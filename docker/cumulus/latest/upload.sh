96f4437b841304d9dd001bd5f6a67a6fe8c6bba0
Yiming Yang <yang.yihming@gmail.com>
2019-12-08 16:23:59 -0500

    rename latest to 0.12.0

--- docker/cumulus/latest/upload.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-docker tag cumulus-latest cumulusprod/cumulus:latest
-docker login
-docker push cumulusprod/cumulus:latest
-docker tag cumulus-latest quay.io/cumulus/cumulus:latest
-docker login quay.io
-docker push quay.io/cumulus/cumulus:latest
