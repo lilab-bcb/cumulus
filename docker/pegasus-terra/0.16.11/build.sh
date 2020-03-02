bbddcdc0ba98b5fe1b54d8c94f3a28111e628ad2
Yiming Yang <yang.yihming@gmail.com>
2020-03-02 12:14:10 -0500

    update pegasus terra docker

---
+++ docker/pegasus-terra/0.16.11/build.sh
@@ -0,0 +1,1 @@
+docker build -t pegasus-terra-0.16.11 .
--- docker/pegasus-terra/0.16.9/upload.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-docker tag pegasus-terra-0.16.9 cumulusprod/pegasus-terra:0.16.9
-docker login
-docker push cumulusprod/pegasus-terra:0.16.9
-docker tag pegasus-terra-0.16.9 quay.io/cumulus/pegasus-terra:0.16.9
-docker login quay.io
-docker push quay.io/cumulus/pegasus-terra:0.16.9
