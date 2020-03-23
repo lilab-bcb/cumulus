4f966daa065b8c55a02fa9d9567e0215fe9a1abe
Yiming Yang <yang.yihming@gmail.com>
2020-03-23 12:25:16 -0400

    change extract barcodes script

--- docker/souporcell/extract_barcodes_for_souporcell.py
+++ docker/souporcell/extract_barcodes_for_souporcell.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
 from sys import argv, exit
-import pegasus as pg
+import pegasusio
 
 if len(argv) != 4:
 	print("Usage: python extract_barcodes_for_souporcell.py input_raw.h5 output_barcodes.tsv ngene")
 	exit(-1)
 
-adata = pg.read_input(argv[1])
-idx = adata.X.getnnz(axis = 1) >= int(argv[3])
+data = pegasusio.read_input(argv[1], ngene = int(argv[3]))
 
 with open(argv[2], "w") as fout:
-	fout.write('\n'.join([x + '-1' for x in adata.obs_names[idx]]) + '\n')
+	fout.write('\n'.join([x + '-1' for x in data.obs_names]) + '\n')
