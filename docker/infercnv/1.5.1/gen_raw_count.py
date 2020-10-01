71c4f2eb09ed21bae6bdf6796f1ba93b5e17fea6
Yiming Yang <yang.yihming@gmail.com>
2020-09-30 23:39:12 -0400

    improve raw count generation

--- docker/infercnv/1.5.1/gen_raw_count.py
+++ docker/infercnv/1.5.1/gen_raw_count.py
@@ -3,16 +3,12 @@ import pegasusio as io
 import pandas as pd
 import numpy as np
 
-all_counts_file = sys.argv[1]
-used_counts_file = sys.argv[2]
-out_name = sys.argv[3]
+counts_file = sys.argv[1]
+out_name = sys.argv[2]
 
-adata = io.read_input(all_counts_file)
-bdata = io.read_input(used_counts_file)
-
-# Use only cells and genes considered in analysis.
-adata = adata[bdata.obs_names, bdata.var_names].copy()
+data = io.read_input(counts_file)
+data.select_matrix('raw.X')
 
 # Get raw count matrix.
-df_rc = pd.DataFrame(data=np.transpose(adata.X.toarray().astype('int')), index=adata.var['featureid'], columns=adata.obs_names)
+df_rc = pd.DataFrame(data=np.transpose(data.X.toarray().astype('int')), index=data.var['featureid'], columns=data.obs_names)
 df_rc.to_csv(f"{out_name}.csv", sep='\t')
