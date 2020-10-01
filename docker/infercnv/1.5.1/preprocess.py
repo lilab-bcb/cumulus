70a48ad04cf4293c075e27075370e3054de61a0e
Yiming Yang <yang.yihming@gmail.com>
2020-10-01 00:03:33 -0400

    merge scripts into one preprocess file

--- docker/infercnv/1.5.1/preprocess.py
+++ docker/infercnv/1.5.1/preprocess.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 import pandas as pd
 import pegasus as pg
 
@@ -6,6 +7,10 @@ zarr_file = sys.argv[1]
 out_name = sys.argv[2]
 
 data = pg.read_input(f"{zarr_file}")
+data.select_matrix('raw.X')
+
+df_raw_count = pd.DataFrame(data=np.transpose(data.X.toarray()), index=data.var['featureid'], columns=data.obs_names)
+df_raw_count.to_csv(f"{out_name}.csv", sep='\t')
 
 df_anno = pd.DataFrame({'cell': data.obs_names, 'anno': data.obs['anno'].values})
 df_anno.to_csv(f"{out_name}.sample_annotation.csv", sep='\t', header=False, index=False)
