97bd48ef78b3fc24cbb1fe66d79247077eb67c9d
Bo Li <bli28@mgh.harvard.edu>
2021-01-07 23:36:19 -0500

    reorganized cellranger docker files

--- docker/cellranger/3.1.0/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Download cellranger-3.1.0.tar.gz into this directory from cell ranger website before building
-docker build -t cellranger-3.1.0 .
-docker tag cellranger-3.1.0 "$DOCKER"/cellranger:3.1.0
-docker push "$DOCKER"/cellranger:3.1.0
