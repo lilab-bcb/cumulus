bc16cb2281d9684b171938b225140373ff2eb8a7
Asma Bankapur <asmariyaz23@gmail.com>
2022-01-18 10:35:33 -0500

    correcting remove prefix script

--- docker/shareseq/shareseq_mkfastq/scripts/remove_prefix.sh
+++ docker/shareseq/shareseq_mkfastq/scripts/remove_prefix.sh
@@ -14,28 +14,20 @@ readarray rvs_arr < <(find $1 -maxdepth 1 -type f -name '__rvs_*.fastq.gz' -prin
 
 if [ ${#fwd_arr[@]} -gt 0  ] && [ ${#rvs_arr[@]} -eq 0 ]
 then
-for fastq in ${fwd_arr[@]}
-do
-    fastqnew=`cut -c 6-11 --complement <<< $fastq`
-    mv $fastq $fastqnew
-done
+    remove_prefix ${fwd_arr[@]}
 elif [ ${#fwd_arr[@]} -eq 0  ] && [ ${#rvs_arr[@]} -gt 0 ]
 then
-for fastq in ${rvs_arr[@]}
-do
-    fastqnew=`cut -c 6-11 --complement <<< $fastq`
-    mv $fastq $fastqnew
-done
+    remove_prefix ${rvs_arr[@]}
 elif [ ${#fwd_arr[@]} -gt 0 ] && [ ${#rvs_arr[@]} -gt 0  ]
 then
-fwd_size=`du -c ${fwd_arr[@]} | tail -n 1 | cut -f 1`
-rvs_size=`du -c ${rvs_arr[@]} | tail -n 1 | cut -f 1`
-if [ $fwd_size -gt $rvs_size ]
-then
-    remove_prefix ${fwd_arr[@]}
-    rm -f ${rvs_arr[@]}
-else
-    remove_prefix ${rvs_arr[@]}
-    rm -f ${fwd_arr[@]}
-fi
+    fwd_size=`du -c ${fwd_arr[@]} | tail -n 1 | cut -f 1`
+    rvs_size=`du -c ${rvs_arr[@]} | tail -n 1 | cut -f 1`
+    if [ $fwd_size -gt $rvs_size ]
+    then
+        remove_prefix ${fwd_arr[@]}
+        rm -f ${rvs_arr[@]}
+    else
+        remove_prefix ${rvs_arr[@]}
+        rm -f ${fwd_arr[@]}        
+    fi
 fi
